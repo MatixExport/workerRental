ed567ee43631f6cf5a9b1db8fd07297bf880cd69
Jakub Kubiś <247712@edu.p.lodz.pl>
2024-11-28 23:28:17 +0100

    add rent list, finish rent views

---
+++ DTO/src/main/java/indie/outsource/rent/FinishRentDTO.java
@@ -0,0 +1,18 @@
+package indie.outsource.rent;
+
+import jakarta.validation.constraints.FutureOrPresent;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class FinishRentDTO {
+
+    UUID rentID;
+}
