6e209b483b2b6f359c0c5b721952ae7bf2aa6464
247662 Gieczyski Mateusz <mateusz.gielczynski@outlook.com>
2024-12-16 18:51:18 +0100

    add links, some styles, refactor user details

--- FRONT/src/components/Rent.svelte
+++ FRONT/src/components/Rent.svelte
@@ -15,6 +15,6 @@
     {/if}
     workerId: {rent.workerID}<br>
     {#if rent.endDate === null}
-        <input type="button" value="finish" onclick={()=>finishRent(rent.id)} class="basis-1/2 border-black border-2 border-solid bg-red-300 p-2 hover:bg-red-500">
+        <input type="button" value="finish" onclick={()=>{finishRent(rent.id);}} class="basis-1/2 border-black border-2 border-solid bg-red-300 p-2 hover:bg-red-500">
     {/if}
 </p>
