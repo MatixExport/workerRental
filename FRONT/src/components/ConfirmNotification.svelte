3a4c22b6492721f99c9476086b0f5e069092bce7
247662 Gieczyski Mateusz <mateusz.gielczynski@outlook.com>
2024-12-19 18:47:50 +0100

    add confirm dialogue for create rent

---
+++ FRONT/src/components/ConfirmNotification.svelte
@@ -0,0 +1,11 @@
+<script>
+    let { message, accept, cancel } = $props();
+</script>
+
+<div class="w-full h-full absolute bg-black bg-opacity-50 top-0 left-0">
+    <div class="w-1/2 h-1/2 top-1/4 left-1/4 absolute bg-white border-red-500 p-20 rounded-2xl">
+        <p>{message}</p>
+        <button class="p-2 bg-gray-400 m-2" onclick={accept}>Accept</button>
+        <button class="p-2 bg-gray-400 m-2" onclick={cancel}>Cancel</button>
+    </div>
+</div>
---
+++ FRONT/src/components/Notification.svelte
@@ -0,0 +1,10 @@
+<script>
+    let { message, callback } = $props();
+</script>
+
+<div class="w-full h-full absolute bg-black bg-opacity-50 top-0 left-0">
+    <div class="w-1/2 h-1/2 top-1/4 left-1/4 absolute bg-white border-red-500 p-20 rounded-2xl">
+        <p>{message}</p>
+        <button class="p-2 bg-gray-400 m-2" onclick={callback}>Ok</button>
+    </div>
+</div>
