a160e18416ce8f443a3c223985ce24cc545429de
Jakub Kubiś <jakubkubis45@gmail.com>
2024-12-26 13:43:22 +0100

    create mobile version using capacitor

---
+++ FRONT/android/app/src/main/java/rental/worker/MainActivity.java
@@ -0,0 +1,5 @@
+package rental.worker;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ FRONT/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'rental.worker',
+  appName: 'WorkerRental',
+  webDir: 'dist'
+};
+
+export default config;
