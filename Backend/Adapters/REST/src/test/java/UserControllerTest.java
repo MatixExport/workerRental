738f44819e2b13a66feb14076d8fc3d3b9fa1c70
Mateusz Giełczyński <mateusz.gielczynski@outlook.com>
2025-03-21 22:05:33 +0100

    add more UserController mocked tests

--- Backend/Adapters/REST/src/test/java/UserControllerTest.java
+++ Backend/Adapters/REST/src/test/java/UserControllerTest.java
@@ -7,6 +7,7 @@ import static org.hamcrest.Matchers.equalTo;
 import exceptions.UserAlreadyExistsException;
 import indie.outsource.user.CreateUserDTO;
 import io.restassured.http.ContentType;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 
 import io.restassured.module.mockmvc.RestAssuredMockMvc;
 import org.junit.jupiter.api.BeforeEach;
@@ -17,12 +18,12 @@ import org.mockito.Mock;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.mockito.Mockito;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import spring.controllers.UserController;
 import view.UserService;
 
+import java.util.List;
 import java.util.UUID;
 
 
@@ -30,6 +31,7 @@ import java.util.UUID;
 @WebMvcTest(UserController.class)
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = RestTestConfiguration.class)
+@AutoConfigureMockMvc
 class UserControllerTest {
     private String baseUri = "/users";
 
@@ -45,6 +47,34 @@ class UserControllerTest {
     }
 
     @Test
+    void getAllUserWhenEmptyTest() {
+        Mockito.when(userService.findAll()).thenReturn(List.<UserEnt>of());
+
+        RestAssuredMockMvc.given()
+                .when()
+                .get(baseUri)
+                .then()
+                .statusCode(HttpStatus.OK.value())
+                .contentType(ContentType.JSON)
+                .body(equalTo("[]"));
+    }
+
+    @Test
+    void getAllUsersTest() {
+        UserEnt userEnt = RestModelFactory.getClientEnt();
+        UserEnt userEnt2 = RestModelFactory.getClientEnt();
+        Mockito.when(userService.findAll()).thenReturn(List.of(userEnt, userEnt2));
+
+        RestAssuredMockMvc.given()
+                .when()
+                .get(baseUri)
+                .then()
+                .statusCode(HttpStatus.OK.value())
+                .contentType(ContentType.JSON)
+                .body("id", equalTo(List.of(userEnt.getId().toString(), userEnt2.getId().toString())));
+    }
+
+    @Test
     void getUserTest() throws ResourceNotFoundException {
         UserEnt userEnt = RestModelFactory.getClientEnt();
         Mockito.when(userService.findById(Mockito.any(UUID.class))).thenReturn(userEnt);
@@ -57,14 +87,24 @@ class UserControllerTest {
                 .contentType(ContentType.JSON)
                 .body("id", equalTo(userEnt.getId().toString()));
     }
+
+    @Test
+    void getUserWhenNotFoundTest() throws ResourceNotFoundException {
+        Mockito.when(userService.findById(Mockito.any(UUID.class))).thenThrow(ResourceNotFoundException.class);
+        RestAssuredMockMvc.given()
+                .when()
+                .get(baseUri+"/"+UUID.randomUUID().toString())
+                .then()
+                .statusCode(HttpStatus.NOT_FOUND.value());
+    }
+
     @Test
     void createUserTest() throws UserAlreadyExistsException {
         UserEnt userEnt = RestModelFactory.getClientEnt();
         Mockito.when(userService.save(Mockito.any(UserEnt.class))).thenReturn(userEnt);
         CreateUserDTO createUserDTO = RestModelFactory.getCreateUserDTO();
 
-        RestAssuredMockMvc
-                .given()
+        RestAssuredMockMvc.given()
                 .contentType("application/json")
                 .body(createUserDTO)
                 .post(baseUri)
@@ -79,8 +119,7 @@ class UserControllerTest {
         Mockito.when(userService.save(Mockito.any(UserEnt.class))).thenThrow(UserAlreadyExistsException.class);
         CreateUserDTO createUserDTO = RestModelFactory.getCreateUserDTO();
 
-        RestAssuredMockMvc
-                .given()
+        RestAssuredMockMvc.given()
                 .contentType("application/json")
                 .body(createUserDTO)
                 .post(baseUri)
